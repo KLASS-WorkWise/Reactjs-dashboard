92c6ba98342066b8325a5701dbb2c8c8196af395
thanhhoan99 <thanhhoan7878647@gmail.com>
2025-09-25 15:14:14 +0700

    update

--- reactjs-app/src/modules/employeeJobApplicant/JobApplicantsPage.tsx
+++ reactjs-app/src/modules/employeeJobApplicant/JobApplicantsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { applicantService } from "../../services/applicant.service";
